16e5d2e57d7dd096f06566fdeac626bf61cdcb21
Noah Ratcliff <me@nratcliff.com>
2019-02-16 08:20:50 -0500

    remove unused usings

--- Assets/Scripts/NetScoreZone.cs
+++ Assets/Scripts/NetScoreZone.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NetScoreZone : MonoBehaviour
 {
@@ -13,19 +11,19 @@ public class NetScoreZone : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         // make sure the object is a basketball, and that it is moving down
-        if (collider.CompareTag("Basketball") 
+        if (collider.CompareTag("Basketball")
             && collider.GetComponent<Rigidbody2D>().velocity.y < 0)
         {
             Debug.Log("Basketball entered the net!");
